ffeddc9e71e5295d7bcbab7ad294be0ffe451471
arpita-32 <arpitasundaray9@gmail.com>
2025-01-08 11:56:15 +0530

    added emailuploader file

---
+++ SERVER/controllers/dist/Course.dev.js
@@ -0,0 +1,8 @@
+"use strict";
+
+var Course = require("../models/Course");
+
+var Tag = require("../models/tags");
+
+var User = require("../models/User");
+//# sourceMappingURL=Course.dev.js.map
---
+++ SERVER/utils/dist/imageUploader.dev.js
@@ -0,0 +1,37 @@
+"use strict";
+
+var cloudinary = require('cloudinary').v2;
+
+exports.uploadImageToCloudinary = function _callee(file, folder, innerHeight, quality) {
+  var options;
+  return regeneratorRuntime.async(function _callee$(_context) {
+    while (1) {
+      switch (_context.prev = _context.next) {
+        case 0:
+          options = {
+            folder: folder
+          };
+
+          if (height) {
+            options.height = height;
+          }
+
+          if (quality) {
+            options.quality = quality;
+          }
+
+          options.resource_type = "auto";
+          _context.next = 6;
+          return regeneratorRuntime.awrap(cloudinary.uploader.upload(file.tempFilePath, options));
+
+        case 6:
+          return _context.abrupt("return", _context.sent);
+
+        case 7:
+        case "end":
+          return _context.stop();
+      }
+    }
+  });
+};
+//# sourceMappingURL=imageUploader.dev.js.map
