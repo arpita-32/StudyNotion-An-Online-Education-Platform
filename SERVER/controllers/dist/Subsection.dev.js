3e3e0a346d2d64a375099343c96fda19c2af91b5
arpita-32 <arpitasundaray9@gmail.com>
2025-01-13 07:22:07 +0530

    updated code

--- SERVER/controllers/dist/Subsection.dev.js
+++ SERVER/controllers/dist/Subsection.dev.js
@@ -79,4 +79,153 @@ exports.createSubSection = function _callee(req, res) {
     }
   }, null, null, [[0, 17]]);
 }; //update and delete section
+
+
+exports.updateSubSection = function _callee2(req, res) {
+  var _req$body2, sectionId, subSectionId, title, description, subSection, video, uploadDetails, updatedSection;
+
+  return regeneratorRuntime.async(function _callee2$(_context2) {
+    while (1) {
+      switch (_context2.prev = _context2.next) {
+        case 0:
+          _context2.prev = 0;
+          _req$body2 = req.body, sectionId = _req$body2.sectionId, subSectionId = _req$body2.subSectionId, title = _req$body2.title, description = _req$body2.description;
+          _context2.next = 4;
+          return regeneratorRuntime.awrap(SubSection.findById(subSectionId));
+
+        case 4:
+          subSection = _context2.sent;
+
+          if (subSection) {
+            _context2.next = 7;
+            break;
+          }
+
+          return _context2.abrupt("return", res.status(404).json({
+            success: false,
+            message: "SubSection not found"
+          }));
+
+        case 7:
+          if (title !== undefined) {
+            subSection.title = title;
+          }
+
+          if (description !== undefined) {
+            subSection.description = description;
+          }
+
+          if (!(req.files && req.files.video !== undefined)) {
+            _context2.next = 16;
+            break;
+          }
+
+          video = req.files.video;
+          _context2.next = 13;
+          return regeneratorRuntime.awrap(uploadImageToCloudinary(video, process.env.FOLDER_NAME));
+
+        case 13:
+          uploadDetails = _context2.sent;
+          subSection.videoUrl = uploadDetails.secure_url;
+          subSection.timeDuration = "".concat(uploadDetails.duration);
+
+        case 16:
+          _context2.next = 18;
+          return regeneratorRuntime.awrap(subSection.save());
+
+        case 18:
+          _context2.next = 20;
+          return regeneratorRuntime.awrap(Section.findById(sectionId).populate("subSection"));
+
+        case 20:
+          updatedSection = _context2.sent;
+          console.log("updated section", updatedSection);
+          return _context2.abrupt("return", res.json({
+            success: true,
+            message: "Section updated successfully",
+            data: updatedSection
+          }));
+
+        case 25:
+          _context2.prev = 25;
+          _context2.t0 = _context2["catch"](0);
+          console.error(_context2.t0);
+          return _context2.abrupt("return", res.status(500).json({
+            success: false,
+            message: "An error occurred while updating the section"
+          }));
+
+        case 29:
+        case "end":
+          return _context2.stop();
+      }
+    }
+  }, null, null, [[0, 25]]);
+};
+
+exports.deleteSubSection = function _callee3(req, res) {
+  var _req$body3, subSectionId, sectionId, subSection, updatedSection;
+
+  return regeneratorRuntime.async(function _callee3$(_context3) {
+    while (1) {
+      switch (_context3.prev = _context3.next) {
+        case 0:
+          _context3.prev = 0;
+          _req$body3 = req.body, subSectionId = _req$body3.subSectionId, sectionId = _req$body3.sectionId;
+          _context3.next = 4;
+          return regeneratorRuntime.awrap(Section.findByIdAndUpdate({
+            _id: sectionId
+          }, {
+            $pull: {
+              subSection: subSectionId
+            }
+          }));
+
+        case 4:
+          _context3.next = 6;
+          return regeneratorRuntime.awrap(SubSection.findByIdAndDelete({
+            _id: subSectionId
+          }));
+
+        case 6:
+          subSection = _context3.sent;
+
+          if (subSection) {
+            _context3.next = 9;
+            break;
+          }
+
+          return _context3.abrupt("return", res.status(404).json({
+            success: false,
+            message: "SubSection not found"
+          }));
+
+        case 9:
+          _context3.next = 11;
+          return regeneratorRuntime.awrap(Section.findById(sectionId).populate("subSection"));
+
+        case 11:
+          updatedSection = _context3.sent;
+          return _context3.abrupt("return", res.json({
+            success: true,
+            message: "SubSection deleted successfully",
+            data: updatedSection
+          }));
+
+        case 15:
+          _context3.prev = 15;
+          _context3.t0 = _context3["catch"](0);
+          console.error(_context3.t0);
+          return _context3.abrupt("return", res.status(500).json({
+            success: false,
+            message: "An error occurred while deleting the SubSection"
+          }));
+
+        case 19:
+        case "end":
+          return _context3.stop();
+      }
+    }
+  }, null, null, [[0, 15]]);
+};
 //# sourceMappingURL=Subsection.dev.js.map
