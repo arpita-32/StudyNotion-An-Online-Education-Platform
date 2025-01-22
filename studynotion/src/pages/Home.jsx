7acd0c4582b7d1ef6a6ad3ff955663261ea0f095
arpita-32 <arpitasundaray9@gmail.com>
2025-01-22 18:13:30 +0530

    added components

--- studynotion/src/pages/Home.jsx
+++ studynotion/src/pages/Home.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { FaArrowRight } from "react-icons/fa";
+import HighlightText from '../components/core/HomePage/HighlightText';
+import CTAButton from '../components/core/HomePage/Button'
+import Banner from "../assets/Images/banner.mp4"
+import CodeBlocks from '../components/core/HomePage/CodeBlocks';
 
 
 const Home = () => {
 return (
     <div>
-    {/*Section 1 */}
-    <div className='relative mx-auto flex flex-col w-11/12 items-center
+    
+    <div className='relative mx-auto flex flex-col w-11/12 max-w-maxContent items-center
     text-white justify-between'>
         <Link to ={"/signup"}>
         <div className='group mt-16 p-1 mx-auto rounded-full bg-richblack-800 font-bold text-richblack-200
@@ -21,9 +25,105 @@ return (
         </div>
         
         </Link>
+
+        <div className='text-center text-4xl font-semibold mt-7'>
+            Empower your Future With 
+            <HighlightText  text = {"Coding Skills"}/>
+        </div>
+        <div className='mt-4 w-[90%] text-center text-lg font-bold text-richblack-300'>
+        With our online coding courses, you can learn at your own pace, from
+        anywhere in the world, and get access to a wealth of resources,
+        including hands-on projects, quizzes, and personalized feedback from
+        instructors.
+        </div>
+
+        <div className='flex flex-row gap-7 mt-8'>
+            <CTAButton active={true} linkto={"/signup"}>
+                Learn More
+            </CTAButton>
+            <CTAButton active={false} linkto={"/signup"}>
+                Book a demo
+            </CTAButton>
+        </div>
+
+        <div className='mx-3 my-12 shadow-blue-200'>
+            <video
+            muted
+            loop
+            autoPlay>
+            <source src={Banner} type='video/mp4'/>
+            </video>
+        </div>
+    </div>
+    {/*Section 1 */}
+    <div>
+        <CodeBlocks
+        position={"lg:flex-row"}
+        heading={
+            <div className='text-4xl font-semibold'>
+                Unlock Your
+                <HighlightText text={"coding potential"}/>
+                with our online courses
+            </div>
+        }
+        subheading={"Our courses are designed and taught by industry experts who have years of experience in coding and are passionate about sharing their knowledge with you."
+        }
+
+        ctabtn1={
+            {
+                btnText:"try it yourself",
+                linkto:"/signup",
+                active: true,
+            }
+        }
+
+        ctabtn2={
+            {
+                btnText:"learn more",
+                linkto:"/login",
+                active: false,
+            }
+        }
+        codeColor={"text-yellow-25"}
+        codeblock={`<!DOCTYPE html>\n <html lang="en">\n<head>\n<title>This is myPage</title>\n</head>\n<body>\n<h1><a href="/">Header</a></h1>\n<nav> <a href="/one">One</a> <a href="/two">Two</a> <a href="/three">Three</a>\n</nav>\n</body>`}
+        backgroundGradient={<div className="codeblock1 absolute"></div>}
+        />
     </div>
 
     {/*Section 2 */}
+    <div>
+        <CodeBlocks
+        position={"lg:flex-row-reverse"}
+        heading={
+            <div className='text-4xl font-semibold'>
+                Unlock Your
+                <HighlightText text={"coding potential"}/>
+                with our online courses
+            </div>
+        }
+        subheading={"Our courses are designed and taught by industry experts who have years of experience in coding and are passionate about sharing their knowledge with you."
+        }
+
+        ctabtn1={
+            {
+                btnText:"try it yourself",
+                linkto:"/signup",
+                active: true,
+            }
+        }
+
+        ctabtn2={
+            {
+                btnText:"learn more",
+                linkto:"/login",
+                active: false,
+            }
+        }
+        codeColor={"text-yellow-25"}
+        codeblock={`<!DOCTYPE html>\n <html lang="en">\n<head>\n<title>This is myPage</title>\n</head>\n<body>\n<h1><a href="/">Header</a></h1>\n<nav> <a href="/one">One</a> <a href="/two">Two</a> <a href="/three">Three</a>\n</nav>\n</body>`}
+        backgroundGradient={<div className="codeblock1 absolute"></div>}
+        />
+    </div>
 
     {/*Section 3 */}
 
