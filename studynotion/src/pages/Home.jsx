07b09e763814e062df86c8147f318014eca07f8a
arpita-32 <arpitasundaray9@gmail.com>
2025-01-23 14:56:41 +0530

    added components

--- studynotion/src/pages/Home.jsx
+++ studynotion/src/pages/Home.jsx
@@ -5,8 +5,8 @@ import HighlightText from '../components/core/HomePage/HighlightText';
 import CTAButton from '../components/core/HomePage/Button'
 import Banner from "../assets/Images/banner.mp4"
 import CodeBlocks from '../components/core/HomePage/CodeBlocks';
-
-
+import LearningLnguageSection from '../components/core/HomePage/LearningLnguageSection';
+import TimelineSection from '../components/core/HomePage/TimelineSection';
 const Home = () => {
 return (
     <div>
@@ -90,7 +90,7 @@ return (
         />
     </div>
 
-    {/*Section 2 */}
+    {/*Code Section 2 */}
     <div>
         <CodeBlocks
         position={"lg:flex-row-reverse"}
@@ -125,7 +125,68 @@ return (
         />
     </div>
 
-    {/*Section 3 */}
+    {/*Section 2 */}
+    <div className='bg-pure-greys-5 text-richblack-700'>
+        <div className='homepage_bg h-[310px]'>
+            <div className='w-11/12 max-w-maxContent flex flex-col items-center justify-between gap-5 mx-auto'>
+            <div className='h-[150px]'></div>
+            <div className='flex flex-row gap-7 text-white'>
+                <CTAButton active={true} linkto={"/signup"}>
+                <div className='flex items-center gap-3'>
+                    Explore full catalog
+                    <FaArrowRight/>
+                </div>
+                
+
+                </CTAButton>
+                <CTAButton active={false} linkto={"/signup"}>
+                <div>
+                    Learn More
+                </div>
+
+                </CTAButton>
+
+            </div>
+
+            </div>
+
+        </div>
+
+        <div className='mx-auto w-11/12 max-w-maxContent flex flex-col items-center justify-between 
+        gap-7'>
+
+            <div className='flex flex-row gap-5 mb-10 mt-[95px]'>
+                <div className='text-4xl font-semibold w-[45%]'>
+                    Get the skills you need for 
+                    <HighlightText text={"Job that is in demand"}/>
+
+                </div>
+                <div className='flex flex-col gap-10 w-[40%] items-start'>
+                    <div className='text-[16px]'>
+                    The modern StudyNotion is the dictates its own terms. Today, to
+                be a competitive specialist requires more than professional
+                skills.
+
+                    </div>
+                    <CTAButton active={true} linkto={"/signup"}>
+                    <div>
+                        Learn More
+                    </div>
+
+                    </CTAButton>
+                </div>
+            </div>
+
+
+
+
+
+
+        </div>
+
+        <TimelineSection/>
+        <LearningLnguageSection/>
+    </div>
 
 
     {/*footer */}
