eb897508a44e8fc3e950c5cff08b0fe602763c2d
arpita-32 <arpitasundaray9@gmail.com>
2025-01-30 07:34:42 +0530

    added login and signup page

--- studynotion/src/App.js
+++ studynotion/src/App.js
@@ -1,6 +1,9 @@
 import './App.css';
 import { Route,Routes } from 'react-router-dom';
 import Home from "./pages/Home"
+import Login from "./pages/Login"
+import Signup from "./pages/Signup"
+
 import Navbar from './components/common/Navbar';
 function App() {
   return (
@@ -8,6 +11,10 @@ function App() {
       <Navbar/>
     <Routes>
       <Route path ="/" element={<Home/>}></Route>
+      <Route path ="/login" element={<Login/>}></Route>
+      <Route path ="/signup" element={<Signup/>}></Route>
+
+
     </Routes>
     </div>
   );
--- studynotion/src/services/api.js
+++ studynotion/src/services/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = ProcessingInstruction.env.REACT_APP_BASE_URL
+const BASE_URL = process.env.REACT_APP_BASE_URL
 
 export const categories = {
     CATEGORIES_API: BASE_URL + "/course/showAllCategories",
