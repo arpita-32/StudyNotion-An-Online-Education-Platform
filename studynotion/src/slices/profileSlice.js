ddad3b1cd4ac492a4c393828ed08aeb451488e04
arpita-32 <arpitasundaray9@gmail.com>
2025-02-07 23:04:09 +0530

    updated dashboard

--- studynotion/src/slices/profileSlice.js
+++ studynotion/src/slices/profileSlice.js
@@ -1,17 +1,22 @@
 import {createSlice} from "@reduxjs/toolkit"
 
 const initialState = {
-        user:null,
+    user: localStorage.getItem("user") ? JSON.parse(localStorage.getItem("user")) : null,
+    loading: false,
 };
 
 const profileSlice = createSlice({
     name:"profile",
     initialState: initialState,
     reducers: {
-        setToken(state, value) {
-            state.token = value.payload;
+        setUser(state, value) {
+            state.user = value.payload;
         },
+        setLoading(state, value) {
+            state.loading = value.payload;
+          },
     },
 });
-export const {setUser} = profileSlice.actions;
+
+export const {setUser, setLoading} = profileSlice.actions;
 export default profileSlice.reducer;
