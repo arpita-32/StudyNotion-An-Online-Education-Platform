1c22fdd0eb8dad8bff1dd4530c8545774b12d9fa
arpita-32 <arpitasundaray9@gmail.com>
2025-04-26 14:42:03 +0530

    updated

--- src/services/api.js
+++ src/services/api.js
@@ -19,10 +19,10 @@ export const profileEndpoints = {
 
 // STUDENTS ENDPOINTS
 export const studentEndpoints = {
-  COURSE_PAYMENT_API: BASE_URL + "/payment/create-payment-intent",
-  COURSE_VERIFY_API: BASE_URL + "/payment/verify-payment",
+  COURSE_PAYMENT_API: BASE_URL + "/payment/capturePayment",
+  COURSE_VERIFY_API: BASE_URL + "/payment/verifyPayment",
   SEND_PAYMENT_SUCCESS_EMAIL_API: BASE_URL + "/payment/sendPaymentSuccessEmail",
-}
+};
 
 // COURSE ENDPOINTS
 export const courseEndpoints = {
